6ec78fa47ee29ce964b0ebd9f55a229b68a3ae08
Bjorn Newkirk Grambow <bjorn@primecut.gr>
2016-04-12 16:44:48 +0300

    bin and erbs

---
+++ devenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+export OPENSHIFT_ELASTICSEARCH_DIR=$HOME/work/openshift-cartridge-elasticsearch/
+export OPENSHIFT_REPO_DIR=$HOME/work/openshift-cartridge-elasticsearch/test/repo/
+export OPENSHIFT_DATA_DIR=$HOME/work/openshift-cartridge-elasticsearch/test/data/
